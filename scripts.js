8d2118a4c8d02d28b85d2915d6896faee2b83fe1
alejandrolozanoz <alejandrolozanoz96@gmail.com>
2020-11-17 23:07:25 -0600

    Finished electron animation

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.SphereGeometry( 1, 18, 18 );
+var geometry = new THREE.SphereGeometry( .5, 18, 18 );
 
 var electronGeometry = new THREE.SphereGeometry( .1, 18, 18 );
 
@@ -15,7 +15,7 @@ var materialNeutron = new THREE.MeshPhongMaterial( { color: 0xff0000 } );
 var sphere1 = new THREE.Mesh( geometry, materialProton );
 scene.add( sphere1 );
 sphere1.position.z=0;
-sphere1.position.x=1;
+sphere1.position.x=.5;
 sphere1.position.y=0;
 
 var sphere2 = new THREE.Mesh( geometry, materialNeutron );
@@ -28,25 +28,25 @@ var sphere3 = new THREE.Mesh( geometry, materialProton );
 scene.add( sphere3 );
 sphere3.position.z=0;
 sphere3.position.x=0;
-sphere3.position.y=1;
+sphere3.position.y=.5;
 
 var sphere4 = new THREE.Mesh( geometry, materialNeutron );
 sphere4.position.z=0;
-sphere4.position.x=1;
-sphere4.position.y=1;
+sphere4.position.x=.5;
+sphere4.position.y=.5;
 scene.add( sphere4 );
 
-var sphere5 = new THREE.Mesh( geometry, materialProton );
+var sphere5 = new THREE.Mesh( geometry, materialNeutron );
 scene.add( sphere5 );
-sphere5.position.z=0.33;
-sphere5.position.x=0.33;
-sphere5.position.y=0.33;
+sphere5.position.z=0.165;
+sphere5.position.x=0.165;
+sphere5.position.y=0.165;
 
-var sphere6 = new THREE.Mesh( geometry, materialNeutron );
+var sphere6 = new THREE.Mesh( geometry, materialProton );
 scene.add( sphere6 );
-sphere6.position.z=-.33;
-sphere6.position.x=0.33;
-sphere6.position.y=0.33;
+sphere6.position.z=-.165;
+sphere6.position.x=0.165;
+sphere6.position.y=0.165;
 
 
 var electron1 = new THREE.Mesh( electronGeometry, materialElectron );
@@ -73,6 +73,12 @@ scene.add( electron7 );
 var electron8 = new THREE.Mesh( electronGeometry, materialElectron );
 scene.add( electron8 );
 
+var electron9 = new THREE.Mesh( electronGeometry, materialElectron );
+scene.add( electron9 );
+
+var electron10 = new THREE.Mesh( electronGeometry, materialElectron );
+scene.add( electron10 );
+
 camera.position.z = 5;
 camera.position.y= 0;
 camera.position.x = 0;
@@ -82,37 +88,52 @@ light.position.set( 8, 1, 4 ); //default; light shining from top
 light.castShadow = true; // default false
 scene.add(light);
 
-scene.fog = new THREE.Fog(0xffffff, 0, 20000);
+scene.fog = new THREE.Fog(0x000000, 0, 20000);
 renderer.setClearColor (scene.fog.color, 1);
 
 var t = 0;
+var t2 = 60;
+var t3 = 180;
+var t4 = 240;
+
 var animate = function () {
 	requestAnimationFrame( animate );
-    t += 0.01;
+    t += 0.05;
+    t2 += 0.05;
+    t3 += 0.05;
+    t4 += 0.05;
 
+	// Horizontal
     electron1.position.x = 3*Math.cos(t);
     electron1.position.z = 3*Math.sin(t);
 
-    electron2.position.x = 3*Math.cos(t);
-    electron2.position.y = 3*Math.sin(t);
-
-    electron3.position.y = 3*Math.cos(t);
-    electron3.position.z = 3*Math.sin(t);
-	
 	electron4.position.x = -3*Math.cos(t);
     electron4.position.z = -3*Math.sin(t);
 
-    electron5.position.x = -3*Math.cos(t);
-    electron5.position.y = -3*Math.sin(t);
-
-    electron6.position.y = -3*Math.cos(t);
-	electron6.position.z = -3*Math.sin(t);
+	// Vertical
+	electron2.position.x = 3*Math.cos(t2);
+    electron2.position.y = 3*Math.sin(t2);
 	
-	electron7.position.x = 3*Math.cos(t);
-    electron7.position.y = 3*Math.sin(t);
+    electron5.position.x = -3*Math.cos(t2);
+    electron5.position.y = -3*Math.sin(t2);
 
-	electron8.position.x = 3*Math.cos(t);
-	electron8.position.y = 3*Math.sin(t);
+	// Diagonal 1
+	electron7.position.x = -3*Math.cos(t3);	
+	electron7.position.y = -3*Math.cos(t3);
+	electron7.position.z = -3*Math.sin(t3);
+
+	electron8.position.x = 3*Math.cos(t3);	
+	electron8.position.y = 3*Math.cos(t3);
+	electron8.position.z = 3*Math.sin(t3);
+
+	// Diagonal 2
+	electron9.position.x = -3*Math.cos(t4);	
+	electron9.position.y = 3*Math.cos(t4);
+	electron9.position.z = -3*Math.sin(t4);
+
+	electron10.position.x = 3*Math.cos(t4);	
+	electron10.position.y = -3*Math.cos(t4);
+	electron10.position.z = 3*Math.sin(t4);
 
 	renderer.render( scene, camera );
 };
